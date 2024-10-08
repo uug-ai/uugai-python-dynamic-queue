30b9a81875af93369e493c71067bcd207e723b81
Cedric Verstraeten <cedric@verstraeten.io>
2024-10-08 23:13:53 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ with open('requirements.txt') as f:
     requirements = f.read().splitlines()
 
 setup(
-   name='uugai_python_dynamic_queue',
-   version='2.0.0',
-   description='Queue reader for python abstracting message brokers such as RabbitMQ', 
-   author='uug.ai',
-   author_email='support@uug.ai',
-   long_description=open('README.md').read(),
-   long_description_content_type='text/markdown',
-   packages=find_packages(),
-   install_requires=requirements
+    name='uugai_python_dynamic_queue',
+    version='2.0.0',
+    description='Queue reader for python abstracting message brokers such as RabbitMQ',
+    author='uug.ai',
+    author_email='support@uug.ai',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    install_requires=requirements
 )
